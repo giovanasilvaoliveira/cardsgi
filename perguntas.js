7d6accec7cb1c76be9f7717c94001aa24bfb96d3
Giovana Oliveira <143838386+giovanasilvaoliveira@users.noreply.github.com>
2024-09-17 16:12:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação e como você a utiliza?',
+    'As variáveis são usadas para salvar e recuperar dados, representar valores existentes e atribuir novos. Elas permitem que os programadores se refiram a um valor com um nome representativo em vez de ter que se lembrar desse valor.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos oceanos existem e quais são eles?',
+    '4 oceanos no mundo: Atlântico, Pacífico, Índico e Ártico.'
+)
+
+criaCartao(
+    'Programação',
+    'O que são funções e como você define e chama uma função em JavaScript?',
+    'De modo geral, função é um "subprograma" que pode ser chamado por código externo (ou interno no caso de recursão) à função. Assim como o programa em si, uma função é composta por uma sequência de instruções chamada corpo da função.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Quando você usa "will" e "going to" para falar sobre o futuro?',
+    'No caso de predições futuras, a estutura going to é usada quando temos uma evidência de que algo acontecerá. Enquanto, com will, a predição provavelmente acontecerá.'
+)
